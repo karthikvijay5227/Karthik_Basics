6d477554b2ad3a8bd43182bed2e4708521a5510f
Karthik Vijay <kv52274@gmail.com>
2021-05-19 11:05:33 +0530

    Modified

--- Arrays.java
+++ Arrays.java
@@ -3,7 +3,7 @@ public class Arrays {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int A[]=new int[10];
-        System.out.println("Enter the values for the array:");
+        System.out.println("Enter the values:");
         for(int i=0;i<10;i++)
         {
             A[i]=sc.nextInt();
